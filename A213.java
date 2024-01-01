249f000585cbb801a1d8a3fe16ad33340f074dcc
Anushka Shanker <anushkashanker424@gmail.com>
2024-01-01 12:19:11 +0530

    finall commit

--- A213.java
+++ A213.java
@@ -16,7 +16,7 @@ public class A213
     }
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
+        try(Scanner sc = new Scanner(System.in)){
         System.out.println("Enter the number of rows:");
         int n = sc.nextInt();
 
@@ -28,5 +28,5 @@ public class A213
             }
             System.out.println();
         }
-    }
+    }}
 }
