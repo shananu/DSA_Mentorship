635e694afa7cd510cc13e9c1cb2c2e04fca0645a
Anushka Shanker <anushkashanker424@gmail.com>
2024-01-01 12:02:31 +0530

    final commit

--- A19.java
+++ A19.java
@@ -1,10 +1,10 @@
 
 import java.util.*;
-public class A_1_9
+public class A19
 {
-    public static void main()
+    public static void main(String[] args)
     {
-        Scanner sc=new Scanner(System.in);
+        try(Scanner sc=new Scanner(System.in)){
         System.out.println("Enter first number");
         int a=sc.nextInt();
         System.out.println("Enter second number");
@@ -21,5 +21,5 @@ public class A_1_9
         l=(a*b)/g;
         System.out.println("Greatest Common Divisor= "+g);
         System.out.println("Lowest Common Multiple= "+l);
-    }
+    }}
 }
