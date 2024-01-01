537897560bdeb2312b9899edbe6544ce1e1d7f65
Anushka Shanker <anushkashanker424@gmail.com>
2024-01-01 12:24:01 +0530

    final commit

--- A21.java
+++ A21.java
@@ -1,20 +1,19 @@
- import java.util.*;
-    public class A21
-    {
-        public static void main(String[] args)
+import java.util.*;
+public class A21 
+{
+  public static void main(String[] args)  
+  {
+    try (Scanner sc = new Scanner(System.in)) {
+        System.out.println("Enter the number of rows");
+        int n= sc.nextInt();
+        for(int i=1;i<=n;i++)
         {
-            try (Scanner sc = new Scanner(System.in)) {
-                System.out.println("Enter the number of rows");
-                int n= sc.nextInt();
-                for(int i=1;i<=n;i++)
-                {
-                    for(int j=1;j<=i;j++)
-                    {
-                        System.out.print("* ");
-                    }
-                    System.out.println();
-                }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("* ");
             }
+            System.out.println();
         }
     }
-
+  }
+}
