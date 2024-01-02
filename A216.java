d74b9b5e170756836b11f27d848b4e676987d776
Anushka Shanker <anushkashanker424@gmail.com>
2024-01-03 01:45:29 +0530

    final commit

--- A216.java
+++ A216.java
@@ -8,29 +8,24 @@ public class A216
             System.out.println("Enter a number");
             int n= sc.nextInt();
             int d= (n-1)*2-1;
-            if(n==1)
-                System.out.println(n);
-            else
+            for(int i=1;i<=n;i++)
             {
-                for(int i=1;i<=n;i++)
+                for(int j=1;j<=i;j++)
                 {
-                    for(int j=1;j<=i;j++)
-                    {
                         System.out.print(j+" ");
-                    }
-                    for(int k=1;k<=d;k++)
-                    {
-                        System.out.print("  ");
-                    }
-                    for(int j=i;j>=1;j--)
-                    {   
-                        if(j==n)
-                            continue;
-                        System.out.print(j+" ");
-                    }
-                    d=d-2;
-                    System.out.println();
                 }
+                for(int k=1;k<=d;k++)
+                {
+                    System.out.print("  ");
+                }
+                for(int j=i;j>=1;j--)
+                {   
+                    if(j==n)
+                        continue;
+                    System.out.print(j+" ");
+                }
+                d=d-2;
+                System.out.println();
             }
         }
     }    
